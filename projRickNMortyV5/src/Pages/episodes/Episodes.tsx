5f6524f90e6e99538f01be590568b43679488b7f
andrevictorbf <andrevictor_barcia@hotmail.com>
2024-07-19 12:52:22 -0300

    commit inicial - com rotas possivelmente configuradas... Continuarei depois a configurar as os componentes e irei mergear com a main

---
+++ projRickNMortyV5/src/Pages/episodes/Episodes.tsx
@@ -0,0 +1,11 @@
+
+
+export const Episodes: React.FC = () => {
+    return (
+        <div>
+            Episodios
+        </div>
+    )
+}
+
+export default Episodes;
--- projRickNMortyV5/src/main.tsx
+++ projRickNMortyV5/src/main.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Characters } from './Pages'
+import { Characters, Episodes } from './Pages'
 import { Wrapper } from './Shared/Wrapper/Wrapper'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Wrapper />
-    <Characters />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Wrapper />} >
+          <Route index element={<Characters />} />
+          <Route path="/episodios" element={<Episodes />} />
+          <Route path="/localizacoes" element={<div>Localizacoes</div>} />
+        </Route>
+      </Routes>
+
+
+    </BrowserRouter>
   </React.StrictMode>,
 )
